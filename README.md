d194cbd22c2bfe7fab89d10ec0c1bf2ac3e99510
coffin1337 <108239565+coffin1337@users.noreply.github.com>
2023-01-07 09:22:21 +0000

    push
    ~ remove project

--- README.md
+++
@@ -1,2 +0,0 @@
-# merdahook
-merdahook reborn nigga
--- hw-sdk/globals/hooks/is_loose_file_allowed/is_loose_file_allowed.cpp
+++
@@ -1,6 +0,0 @@
-#include "is_loose_file_allowed.h"
-
-bool __fastcall hooks::is_loose_file_allowed::is_loose_file_allowed_detour( void* ecx, void* edx )
-{
-	return true;
-}
