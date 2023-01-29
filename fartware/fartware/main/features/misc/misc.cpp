f502223603a4a7d158c71487d939d49857f3ddd3
coffin1337 <108239565+coffin1337@users.noreply.github.com>
2023-01-29 09:40:02 +0000

    Update misc.cpp

--- fartware/fartware/main/features/misc/misc.cpp
+++ fartware/fartware/main/features/misc/misc.cpp
@@ -7,59 +7,69 @@ void misc_t::on_paint_traverse( )
 {
 	if ( !render.m_initialised )
 		return;
-	// watermark
+}
+
+void misc_t::on_end_scene( )
+{
 	[ & ]( const bool can_draw_watermark ) {
 		if ( !can_draw_watermark )
 			return;
-
-		// variable spam yippie (TODO: clean this)
 
 		const ImColor white        = ImColor( 1.f, 1.f, 1.f, 1.f );
 		const ImColor accent_color = ImGui::GetColorU32( ImGuiCol_::ImGuiCol_Accent );
 		const ImColor grey         = ImGui::GetColorU32( ImGuiCol_::ImGuiCol_TextDisabled );
 		const ImColor black        = ImColor( 0.f, 0.f, 0.f, 1.f );
 
-		const auto font = render.m_fonts[ e_font_names::font_name_verdana_bd_11 ];
+		static float frame_rate_f = 1.0f;
+		frame_rate_f              = ( 0.9f * frame_rate_f ) + ( 0.1f * memory.m_globals->m_abs_frame_time ); /* TODO ~ store this globally */
 
-		constexpr auto render_text = [ & ]( const c_vector_2d& pos, const std::string text, ImColor color ) {
-			render.m_draw_data.emplace_back(
-				e_draw_type::draw_type_text,
-				std::make_any< text_draw_object_t >( font, pos, text, color, black, e_text_render_flags::text_render_flag_dropshadow ) );
-		};
+		static auto width = 0.f;
 
-		const int fps_text_size = font->CalcTextSizeA( font->FontSize, FLT_MAX, 0.f, "fps" ).x + 5;
+		ImGui::SetNextWindowSize( ImVec2( -1, 29.f ), ImGuiCond_::ImGuiCond_Always );
+		ImGui::SetNextWindowPos( ImVec2( globals.m_display_size.x - width - 5.f, 5.f ), ImGuiCond_::ImGuiCond_Always );
+		ImGui::Begin( ( "hotwheels-practice-window-ui" ), 0,
+		              ImGuiWindowFlags_::ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_::ImGuiWindowFlags_NoScrollbar |
+		                  ImGuiWindowFlags_::ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_::ImGuiWindowFlags_AlwaysAutoResize );
+		{
+			const auto window = ImGui::GetCurrentWindow( );
 
-		static float frame_rate_f       = 1.0f;
-		frame_rate_f                    = ( 0.9f * frame_rate_f ) + ( 0.1f * memory.m_globals->m_abs_frame_time );
-		const std::string fps_number    = std::to_string( frame_rate_f != 0.0f ? static_cast< int >( 1 / frame_rate_f ) : 0 );
-		const auto fps_number_text_size = font->CalcTextSizeA( font->FontSize, FLT_MAX, 0.f, fps_number.c_str( ) );
+			const auto draw_list = window->DrawList;
 
-		const auto vip_text_size       = font->CalcTextSizeA( font->FontSize, FLT_MAX, 0.f, ".vip" );
-		const auto hotwheels_text_size = font->CalcTextSizeA( font->FontSize, FLT_MAX, 0.f, "hotwheels" );
+			const auto size     = window->Size;
+			const auto position = window->Pos;
 
-		render.m_draw_data.emplace_back(
-			e_draw_type::draw_type_rect,
-			std::make_any< rect_draw_object_t >(
-				c_vector_2d( globals.m_display_size.x - fps_text_size - 61 - vip_text_size.x - hotwheels_text_size.x, 10 ),
-				c_vector_2d( globals.m_display_size.x - fps_text_size + 5, 33 ), ImColor( .09f, .09f, .09f, 1.f ), ImColor( 0.f, 0.f, 0.f, 1.f ),
-				true, 2.f ) );
+			/* render outline */
+			ImGui::PushClipRect( ImVec2( position.x + 1.f, position.y + 1.f ), ImVec2( position.x + size.x - 1.f, position.y + size.y - 1.f ),
+			                     false );
+			draw_list->AddRect( ImVec2( position.x + 1.f, position.y + 1.f ), ImVec2( position.x + size.x - 1.f, position.y + size.y - 1.f ),
+			                    ImColor( 50 / 255.f, 50 / 255.f, 50 / 255.f ), ImGui::GetStyle( ).WindowRounding );
+			ImGui::PopClipRect( );
 
-		render_text( c_vector_2d( globals.m_display_size.x - fps_text_size - 15, 15 ), "fps", grey );
+			ImGui::PushFont( render.m_fonts[ e_font_names::font_name_verdana_bd_11 ] );
 
-		render_text( c_vector_2d( globals.m_display_size.x - fps_text_size - 38, 15 ), fps_number, white );
+			ImGui::Text( "hotwheels" );
+			ImGui::SameLine( 0.f, 0.f );
+			ImGui::TextColored( accent_color, ".vip" );
 
-		render.m_draw_data.emplace_back( e_draw_type::draw_type_line,
-		                                 std::make_any< line_object_t >( ImVec2( globals.m_display_size.x - fps_text_size - 42, 12 ),
-		                                                                 ImVec2( globals.m_display_size.x - fps_text_size - 42, 28 ),
-		                                                                 ImColor( 1.f, 1.f, 1.f, 0.1f ), 1.f ) );
-		render_text( c_vector_2d( globals.m_display_size.x - fps_text_size - 50 - vip_text_size.x, 15 ), ".vip", accent_color );
+			ImGui::SameLine( );
 
-		render_text( c_vector_2d( globals.m_display_size.x - fps_text_size - 51 - vip_text_size.x - hotwheels_text_size.x, 15 ), "hotwheels", white );
+			ImGui::TextColored( ImVec4( 1.f, 1.f, 1.f, 0.1f ), "|" ); /* SORRY BOUT dat.. */
+
+			ImGui::SameLine( );
+
+			ImGui::Text( std::to_string( frame_rate_f != 0.0f ? static_cast< int >( 1 / frame_rate_f ) : 0 ).c_str( ) );
+			
+			ImGui::SameLine( );
+
+			ImGui::TextColored( grey, "fps" );
+
+			ImGui::PopFont( );
+
+			width = ImGui::GetWindowSize( ).x;
+		}
+		ImGui::End( );
 	}( GET_CONFIG_BOOL( variables.m_menu.m_watermark ) );
-}
 
-void misc_t::on_end_scene( )
-{
 	// functions that require player to be ingame
 	if ( !globals.m_local || !interfaces.m_engine->is_in_game( ) || globals.m_local->get_observer_mode( ) == e_obs_mode::obs_mode_deathcam )
 		return;
@@ -114,15 +124,14 @@ void misc_t::on_end_scene( )
 				ImGui::PushClipRect( ImVec2( position.x + 1.f, position.y + 1.f ), ImVec2( position.x + size.x - 1.f, position.y + size.y - 1.f ),
 				                     false );
 				draw_list->AddRect( ImVec2( position.x + 1.f, position.y + 1.f ), ImVec2( position.x + size.x - 1.f, position.y + size.y - 1.f ),
-				                    ImColor( 50 / 255.f, 50 / 255.f, 50 / 255.f ),
-				                    ImGui::GetStyle( ).WindowRounding );
+				                    ImColor( 50 / 255.f, 50 / 255.f, 50 / 255.f ), ImGui::GetStyle( ).WindowRounding );
 				ImGui::PopClipRect( );
 			}( );
 
 			ImGui::SetCursorPosY( 30.f );
 
 			if ( !convars.find( fnv1a::hash_const( "sv_cheats" ) )->get_bool( ) ) {
-				ImGui::Text( "You need sv_cheats 1" );
+				ImGui::Text( "sv_cheats 1 is not enabled" );
 			} else {
 				const auto cp_key = GET_CONFIG_BIND( variables.m_misc.m_practice_cp_key ).m_key;
 
@@ -156,12 +165,13 @@ void misc_t::on_create_move_pre( )
 				return;
 			}
 
-			c_angle saved_angles;
+			c_angle saved_angles = { };
 			interfaces.m_engine->get_view_angles( saved_angles );
 
-			practice.saved_angles = saved_angles;
-
+			practice.saved_angles   = saved_angles;
 			practice.saved_position = globals.m_local->abs_origin( );
+
+			g_log.print( "saved checkpoint.", "[practice]" );
 		} else if ( input.is_key_released( tp_key ) ) {
 			if ( practice.saved_angles.is_zero( ) || practice.saved_position.is_zero( ) )
 				return;
@@ -174,6 +184,8 @@ void misc_t::on_create_move_pre( )
 					.append( std::vformat(
 						"{} {} {}", std::make_format_args( practice.saved_angles.m_x, practice.saved_angles.m_y, practice.saved_angles.m_z ) ) )
 					.c_str( ) );
+
+			g_log.print( "teleported to saved checkpoint.", "[practice]" );
 		}
 	}( GET_CONFIG_BOOL( variables.m_misc.m_practice_window ) );
 }
