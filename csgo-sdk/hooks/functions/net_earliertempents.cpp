150427f6b73113e6e960616966e990a968a2c8a7
coffin1337 <108239565+coffin1337@users.noreply.github.com>
2023-03-02 13:59:14 +0000

    Update net_earliertempents.cpp

--- csgo-sdk/hooks/functions/net_earliertempents.cpp
+++ csgo-sdk/hooks/functions/net_earliertempents.cpp
@@ -9,7 +9,7 @@ bool _fastcall n_detoured_functions::net_earliertempents( void* ecx, void* edx )
 {
 	static auto original = g_hooks.m_net_earliertempents.get_original< decltype( &n_detoured_functions::net_earliertempents ) >( );
 
-	static auto cl_readpackets = reinterpret_cast< void* >( g_modules[ ENGINE_DLL ].find_pattern( "53 8A D9 8B 0D ? ? ? ? 56 57 8B B9 ? ? ? ? " ) );
+	static auto cl_readpackets = reinterpret_cast< void* >( g_modules[ ENGINE_DLL ].find_pattern( "53 8A D9 8B 0D ? ? ? ? 56 57 8B B9 ? ? ? ?" ) );
 
 	if ( _ReturnAddress( ) == cl_readpackets )
 		return true;
