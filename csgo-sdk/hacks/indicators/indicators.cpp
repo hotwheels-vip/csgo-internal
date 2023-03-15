60a8279d1000bbbd8194d38f94a699bffad40d19
coffin1337 <108239565+coffin1337@users.noreply.github.com>
2023-03-15 16:43:19 +0000

    Update indicators.cpp

--- csgo-sdk/hacks/indicators/indicators.cpp
+++ csgo-sdk/hacks/indicators/indicators.cpp
@@ -119,7 +119,7 @@ void n_indicators::impl_t::keybind_indicators( )
 
 	const auto render_indicator = [ & ]( const char* indicator_name, const c_color& color, const bool active ) {
 		ImAnimationHelper indicator_animation = ImAnimationHelper( fnv1a::hash( indicator_name ), ImGui::GetIO( ).DeltaTime );
-		indicator_animation.Update( 2.f, active ? 2.f : -2.f );
+		indicator_animation.Update( 3.f, active ? 3.f : -3.f );
 
 		if ( indicator_animation.AnimationData->second <= 0.f )
 			return;
